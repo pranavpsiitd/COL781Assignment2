6da5c135934d68c84184d08cccec16ee16ff00ba
icygurkirat <gkiratbajwa@gmail.com>
2017-02-20 18:35:24 +0530

    Basic Hierarchical Model

--- COL781Assignment2/main.cpp
+++ COL781Assignment2/main.cpp
@@ -1,39 +1,110 @@
-#include "GL\glew.h"
-#include "GL\freeglut.h"
-#include <iostream>
+#include <GL/glut.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-using namespace std;
+static int shoulder = 0, elbow = 0, shoulder_z = 0;
 
-void renderScene(void)
+void init(void)
 {
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glShadeModel(GL_FLAT);
+}
 
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glClearColor(1.0, 0.0, 0.0, 1.0);//clear red
+void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glPushMatrix();
+	glTranslatef(-1.0, 0.0, 0.0);
+	glRotatef((GLfloat)shoulder_z, 0.0, 1.0, 0.0);
+	glRotatef((GLfloat)shoulder, 0.0, 0.0, 1.0);
+	glTranslatef(1.0, 0.0, 0.0);
+	glPushMatrix();
+	glScalef(2.0, 0.4, 1.0);
+	glutWireCube(1.0);
+	glPopMatrix();
 
+	glTranslatef(1.0, 0.0, 0.0);
+	glRotatef((GLfloat)elbow, 0.0, 0.0, 1.0);
+	glTranslatef(1.0, 0.0, 0.0);
+	glPushMatrix();
+	glScalef(2.0, 0.4, 1.0);
+	glutWireCube(1.0);
+	glPopMatrix();
+
+	glPopMatrix();
+	/*glPushMatrix();
+	glutWireSphere(1.0, 20, 16);
+	glRotatef((GLfloat)shoulder, 0.0, 1.0, 0.0);
+	glTranslatef(2.0, 0.0, 0.0);
+	glRotatef((GLfloat)elbow, 0.0, 1.0, 0.0);
+	glutWireSphere(0.2, 10, 8);
+	glPopMatrix();*/
 	glutSwapBuffers();
 }
 
-int main(int argc, char **argv)
+void reshape(int w, int h)
+{
+	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(65.0, (GLfloat)w / (GLfloat)h, 1.0, 20.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glTranslatef(0.0, 0.0, -5.0);
+}
+
+void special(int key, int x, int y)
 {
 
+	printf("%d\n", key);
+
+}
+
+/* ARGSUSED1 */
+void keyboard(unsigned char key, int x, int y)
+{
+	printf("%d\n", key);
+	switch (key) {
+	case 's':
+		shoulder = (shoulder + 5) % 360;
+		glutPostRedisplay();
+		break;
+	case 'S':
+		shoulder = (shoulder - 5) % 360;
+		glutPostRedisplay();
+		break;
+	case 'e':
+		elbow = (elbow + 5) % 360;
+		glutPostRedisplay();
+		break;
+	case 'E':
+		elbow = (elbow - 5) % 360;
+		glutPostRedisplay();
+		break;
+	case 'z':
+		shoulder_z = (shoulder_z + 5) % 360;
+		glutPostRedisplay();
+		break;
+	case 27:
+		exit(0);
+		break;
+	default:
+		break;
+	}
+}
+
+int main(int argc, char** argv)
+{
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-	glutInitWindowPosition(500, 500);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
 	glutInitWindowSize(800, 600);
-	glutCreateWindow("OpenGL First Window");
-
-	glEnable(GL_DEPTH_TEST);
-
-	// register callbacks
-	glutDisplayFunc(renderScene);
-
-	//glutMainLoop();
-
-	cout << glGetString(GL_RENDERER) << endl;
-	cout << glewGetString(GLEW_VERSION) << endl;
-	cout << glGetString(GL_VERSION) << endl;
-	cout << glutGet(GLUT_VERSION) << endl;
-	getchar();
-
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow(argv[0]);
+	init();
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutKeyboardUpFunc(keyboard);
+	glutSpecialUpFunc(special);
+	glutMainLoop();
 	return 0;
 }
